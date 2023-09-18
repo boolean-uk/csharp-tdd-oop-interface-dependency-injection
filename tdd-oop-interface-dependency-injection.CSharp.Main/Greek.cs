7b4f9d759ade55416e8604dbe0818b18fac03058
Annefleur Hay <annefleurhay1@gmail.com>
2023-09-18 16:31:06 +0200

    core

---
+++ tdd-oop-interface-dependency-injection.CSharp.Main/Greek.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_public_interface_dependency_injection.CSharp.Main
+{
+    public class Greek : ILetters
+    {
+        public Dictionary<Char, int> LetterScores 
+        {
+            get
+            {
+                Dictionary<Char, int> GreekWords = new Dictionary<Char, int>();
+                GreekWords.Add('α', 1);
+                GreekWords.Add('ε', 1);
+                GreekWords.Add('ι', 1);
+                GreekWords.Add('ο', 1);
+                GreekWords.Add('φ', 1);
+                GreekWords.Add('ξ', 1);
+                GreekWords.Add('β', 2);
+                GreekWords.Add('δ', 2);
+                GreekWords.Add('π', 2);
+                GreekWords.Add('γ', 3);
+                GreekWords.Add('η', 3);
+                GreekWords.Add('ν', 3);
+                GreekWords.Add('τ', 3);
+                GreekWords.Add('ζ', 4);
+                GreekWords.Add('κ', 4);
+                GreekWords.Add('ρ', 4);
+                GreekWords.Add('ψ', 4);
+                GreekWords.Add('λ', 4);
+                GreekWords.Add('θ', 5);
+                GreekWords.Add('χ', 5);
+                GreekWords.Add('μ', 8);
+                GreekWords.Add('σ', 8);
+                GreekWords.Add('υ', 10);
+                GreekWords.Add('ω', 10);
+                return GreekWords;
+
+
+            }
+
+
+
+
+        }
+    }
+}
--- tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tdd_oop_public_interface_dependency_injection.CSharp.Main;
 
 namespace tdd_oop_interface_dependency_injection.CSharp.Main
 {
     public class Scrabble {
-        private Dictionary<Char, int> letterScores;
+        private Dictionary<Char, int> _letterScores;
 
-        public Scrabble() {
-            Alphabet a = new Alphabet();
-            this.letterScores = a.getLetterScores();
+        public Scrabble(ILetters letters) {
+            
+            this._letterScores = letters.LetterScores;
         }
 
         public int score(String word) {
             int total = 0;
 
             foreach (char ch in word.ToCharArray()) {
-                if (this.letterScores.ContainsKey(ch)) {
-                    int score = this.letterScores[ch];
+                if (this._letterScores.ContainsKey(ch)) {
+                    int score = this._letterScores[ch];
                     total += score;
                 }
             }
