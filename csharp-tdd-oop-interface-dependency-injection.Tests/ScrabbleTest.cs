ab56a308bc7ef80c76ea57d4b2a1962b15b8916e
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-19 11:54:44 +0200

    changed tests to solve the warnings assertions instead of classic model

--- csharp-tdd-oop-interface-dependency-injection.Tests/ScrabbleTest.cs
+++ csharp-tdd-oop-interface-dependency-injection.Tests/ScrabbleTest.cs
@@ -24,121 +24,122 @@ namespace tdd_oop_interface_dependency_injection.CSharp.Test
         [Test]
         public void shouldGive0ForEmptyWords()
         {
-            Assert.AreEqual(0, scrabbles["English"].score(""));
+            //   OR Scrabble scrabble = new Scrabble(new EnglishLetterScores());
+            Assert.That(scrabbles["English"].score(""), Is.EqualTo(0));
         }
 
         [Test]
         public void shouldGive0ForWhiteSpace()
         {
-            Assert.AreEqual(0, scrabbles["English"].score("\n\r\t\b\f"));
+            Assert.That(scrabbles["English"].score("\n\r\t\b\f"), Is.EqualTo(0));
         }
 
         [Test]
         public void shouldScoreBeCaseInsensitively()
         {
-            Assert.AreEqual(scrabbles["English"].score("A"), scrabbles["English"].score("a"));
+            Assert.That(scrabbles["English"].score("A"), Is.EqualTo(scrabbles["English"].score("a")));
         }
 
         [Test]
         public void shouldIgnoreNonAlphabetCharacters()
         {
-            Assert.AreEqual(11, scrabbles["English"].score("a1sdfsd!"));
+            Assert.That(scrabbles["English"].score("a1sdfsd!"), Is.EqualTo(11));
         }
 
         [Test]
         public void shouldIgnoreNonExistentLetter()
         {
-            Assert.AreEqual(0, scrabbles["English"].score("å"));
+            Assert.That(scrabbles["English"].score("å"), Is.EqualTo(0));
         }
 
         [Test]
         public void shouldHandleNullInput() //not for empty string, but for aa null valu 
         {
-            Assert.AreEqual(0, scrabbles["English"].score(null));
+            Assert.That(scrabbles["English"].score(null), Is.EqualTo(0));
         }
 
         [Test]
         public void shouldScore1ForA()
         {
-            Assert.AreEqual(1, scrabbles["English"].score("a"));
+            Assert.That(scrabbles["English"].score("a"), Is.EqualTo(1));
         }
 
         [Test]
         public void shouldScore4ForF()
         {
-            Assert.AreEqual(4, scrabbles["English"].score("f"));
+            Assert.That(scrabbles["English"].score("f"), Is.EqualTo(4));
         }
 
         [Test]
         public void shouldScore6ForStreet()
         {
-            Assert.AreEqual(6, scrabbles["English"].score("street"));
+            Assert.That(scrabbles["English"].score("street"), Is.EqualTo(6));
         }
 
         [Test]
         public void shouldScore22ForQuirky()
         {
-            Assert.AreEqual(22, scrabbles["English"].score("quirky"));
+            Assert.That(scrabbles["English"].score("quirky"), Is.EqualTo(22));
         }
 
         [Test]
         public void shouldScoreRussianLetters()
         {
-            Assert.AreEqual(18, scrabbles["Russian"].score("дврфъ"));
+            Assert.That(scrabbles["Russian"].score("дврфъ"), Is.EqualTo(18));
         }
 
         [Test]
         public void shouldScore1ForRussianA()
         {
-            Assert.AreEqual(1, scrabbles["Russian"].score("а"));
+            Assert.That(scrabbles["Russian"].score("а"), Is.EqualTo(1));
         }
 
         [Test]
         public void shouldScore2ForRussianE()
         {
-            Assert.AreEqual(2, scrabbles["Russian"].score("е"));
+            Assert.That(scrabbles["Russian"].score("е"), Is.EqualTo(2));
         }
 
         [Test]
         public void shouldScore4ForRussianO()
         {
-            Assert.AreEqual(4, scrabbles["Russian"].score("о"));
+            Assert.That(scrabbles["Russian"].score("о"), Is.EqualTo(4));
         }
 
         [Test]
         public void shouldScore10ForRussianYu()
         {
-            Assert.AreEqual(10, scrabbles["Russian"].score("ю"));
+            Assert.That(scrabbles["Russian"].score("ю"), Is.EqualTo(10));
         }
 
         [Test]
         public void shouldScoreGreekLetters()
         {
-            Assert.AreEqual(16, scrabbles["Greek"].score("φεψω"));
+            Assert.That(scrabbles["Greek"].score("φεψω"), Is.EqualTo(16));
         }
 
         [Test]
         public void shouldScore1ForGreekAlpha()
         {
-            Assert.AreEqual(1, scrabbles["Greek"].score("α"));
+            Assert.That(scrabbles["Greek"].score("α"), Is.EqualTo(1));
         }
 
         [Test]
         public void shouldScore2ForGreekBeta()
         {
-            Assert.AreEqual(2, scrabbles["Greek"].score("β"));
+            Assert.That(scrabbles["Greek"].score("β"), Is.EqualTo(2));
         }
 
         [Test]
         public void shouldScore4ForGreekZeta()
         {
-            Assert.AreEqual(4, scrabbles["Greek"].score("ζ"));
+            Assert.That(scrabbles["Greek"].score("ζ"), Is.EqualTo(4));
         }
 
         [Test]
         public void shouldScore10ForGreekOmega()
         {
-            Assert.AreEqual(10, scrabbles["Greek"].score("ω"));
+            Assert.That(scrabbles["Greek"].score("ω"), Is.EqualTo(10));
         }
     }
 }
