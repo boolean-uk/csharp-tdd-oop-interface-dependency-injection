c2a1039e57386e868b91d81a65b6be716bb35863
llllllll-l <vincent.werelius@gmail.com>
2024-01-17 14:11:41 +0100

    Interfaces for all alphabets

---
+++ tdd-oop-interface-dependency-injection.CSharp.Main/IEnglishAlphabet.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tdd_oop_interface_dependency_injection.CSharp.Main
+{
+    public interface IEnglishAlphabet : IAlphabet
+    {
+        
+    }
+}
