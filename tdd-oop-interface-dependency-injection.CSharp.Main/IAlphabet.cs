e22943c0e95ddc93e7e68a96b11480d178d21683
Matthias Greeven <matsgreeven@gmail.com>
2024-01-18 14:07:16 +0100

    Finished exercise

---
+++ tdd-oop-interface-dependency-injection.CSharp.Main/IAlphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_interface_dependency_injection.CSharp.Main
+{
+    public interface IAlphabet
+    {
+        public Dictionary<Char, int> getLetterScores()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
