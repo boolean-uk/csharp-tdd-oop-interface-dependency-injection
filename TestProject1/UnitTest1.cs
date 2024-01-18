c5cafe74d624a3eb6e520c9c7898d6be93d7c41a
Mathias Fredriksen <mathias.fredriksen@no.experis.com>
2024-01-18 14:56:16 +0100

    Task complete. Had to rewrite some things due to issue with .test requiring a main method

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,63 +1,64 @@
-﻿using NUnit.Framework;
-using System.Runtime.InteropServices;
-using tdd_oop_interface_dependency_injection.CSharp.Main;
+﻿using tdd_oop_interface_dependency_injection.CSharp.Main;
 
-namespace tdd_oop_interface_dependency_injection.CSharp.Test
+namespace TestProject1
 {
     [TestFixture]
-    public class ScrabbleTest 
+    public class ScrabbleTest
     {
-
-
         [Test]
-        public void shouldGive0ForEmptyWords() {
+        public void shouldGive0ForEmptyWords()
+        {
             Scrabble scrabble = new Scrabble();
             Assert.That(0, Is.EqualTo(scrabble.score("")));
         }
 
         [Test]
-        public void shouldGive0ForWhiteSpace() {
+        public void shouldGive0ForWhiteSpace()
+        {
             Scrabble scrabble = new Scrabble();
             Assert.That(0, Is.EqualTo(scrabble.score("\n\r\t\b\f")));
         }
 
         [Test]
-        public void shouldScore1ForA() {
+        public void shouldScore1ForA()
+        {
             Scrabble scrabble = new Scrabble();
             Assert.That(1, Is.EqualTo(scrabble.score("a")));
         }
 
         [Test]
-        public void shouldScore4ForF() {
+        public void shouldScore4ForF()
+        {
             Scrabble scrabble = new Scrabble();
             Assert.That(4, Is.EqualTo(scrabble.score("f")));
         }
 
         [Test]
-        public void shouldScore6ForStreet() {
+        public void shouldScore6ForStreet()
+        {
             Scrabble scrabble = new Scrabble();
             Assert.That(6, Is.EqualTo(scrabble.score("street")));
         }
 
         [Test]
-        public void shouldScore22ForQuirky() {
+        public void shouldScore22ForQuirky()
+        {
             Scrabble scrabble = new Scrabble();
             Assert.That(22, Is.EqualTo(scrabble.score("quirky")));
         }
 
         [Test]
-        public void shouldScoreRussianLetters() {
+        public void shouldScoreRussianLetters()
+        {
             Scrabble scrabble = new Scrabble();
             Assert.That(18, Is.EqualTo(scrabble.score("дврфъ")));
         }
 
         [Test]
-        public void shouldScoreGreekLetters() {
+        public void shouldScoreGreekLetters()
+        {
             Scrabble scrabble = new Scrabble();
             Assert.That(20, Is.EqualTo(scrabble.score("φεψωλ")));
         }
-
-
     }
 }
-
--- tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tdd_oop_interface_dependency_injection.Main;
+using tdd_oop_interface_Dependency_injection.CSharp.Main;
 
 namespace tdd_oop_interface_dependency_injection.CSharp.Main
 {
     public class Scrabble {
         private Dictionary<Char, int> letterScores;
 
-        public Scrabble() {
-            Alphabet a = new Alphabet();
-            this.letterScores = a.getLetterScores();
+        public Scrabble()
+        {
+            ILanguagePack english = new AlphabetEnglish();
+            ILanguagePack greek = new AlphabetGreek();
+            ILanguagePack russian = new AlphabetRussian();
+
+            letterScores = english.GetLetterScores();
+            letterScores = letterScores.Concat(greek.GetLetterScores()).ToDictionary(x => x.Key, x => x.Value);
+            letterScores = letterScores.Concat(russian.GetLetterScores()).ToDictionary(x => x.Key, x => x.Value);
         }
 
         public int score(String word) {
