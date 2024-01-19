fa3d1979b03a5f4d8cda44208253faca45cffd09
Mikolaj <mkmbaran@gmail.com>
2024-01-19 16:52:32 +0100

    Improved solution to utilize dependency injection with an interface, and modified tests to match

--- tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
@@ -9,8 +9,8 @@ namespace tdd_oop_interface_dependency_injection.CSharp.Main
     public class Scrabble {
         private Dictionary<Char, int> letterScores;
 
-        public Scrabble() {
-            Alphabet a = new Alphabet();
+        public Scrabble(IAlphabet alphabet) {
+            IAlphabet a = alphabet;
             this.letterScores = a.getLetterScores();
         }
 
