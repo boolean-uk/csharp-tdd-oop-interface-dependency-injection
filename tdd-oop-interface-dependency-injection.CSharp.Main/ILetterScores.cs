a7ec49f5f3a1203e55552d7aa3aa01d7268974de
Louis <louisshrestha0412@outlook.com>
2024-01-19 09:47:30 +0100

    dependency injection

---
+++ tdd-oop-interface-dependency-injection.CSharp.Main/ILetterScores.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface_dependency_injection.CSharp.Main
+{
+    public interface ILetterScores
+    {
+        Dictionary<Char, int> getLetterScores();
+    }
+}
