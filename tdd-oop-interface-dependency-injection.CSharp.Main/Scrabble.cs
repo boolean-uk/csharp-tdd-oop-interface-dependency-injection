3986b4b70d9f26ba8f311a8c2b6d43050bcaf77c
Mateusz Pietrzak <matti3301@gmail.com>
2024-01-19 09:16:44 +0100

    Completed all tasks

--- tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/Scrabble.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tdd_oop_internal_interface_dependency_injection.CSharp.Main;
 
 namespace tdd_oop_interface_dependency_injection.CSharp.Main
 {
     public class Scrabble {
         private Dictionary<Char, int> letterScores;
 
-        public Scrabble() {
-            Alphabet a = new Alphabet();
-            this.letterScores = a.getLetterScores();
+        public Scrabble(IAlphabet alphabet) {
+            this.letterScores = alphabet.getLetterScores();
         }
 
         public int score(String word) {
