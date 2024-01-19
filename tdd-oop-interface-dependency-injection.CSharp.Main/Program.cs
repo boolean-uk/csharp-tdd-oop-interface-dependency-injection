72edf53c493a696ddf87f5f7166c64e6e701f701
Stian Andreas Ytterstad Nordvik <stian.andreas.ytterstad.nordvik@no.experis.com>
2024-01-19 08:36:56 +0100

    Stian Andreas Ytterstad Nordvik C#3

--- tdd-oop-interface-dependency-injection.CSharp.Main/Program.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using tdd_oop_interface_dependency_injection.CSharp.Main;
+
 Console.WriteLine("Hello, World!");
+
+
+Alphabet alphabet = new Alphabet();
+Scrabble scrabble = new Scrabble(alphabet);
