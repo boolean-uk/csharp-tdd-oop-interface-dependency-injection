9bbeb6ca342e4d5a4a8ece2737352c2b227868e7
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-01-21 09:23:46 +0100

    Improved Program with Interfaces and Adjusted Tests

---
+++ tdd-oop-interface-dependency-injection.CSharp.Main/GreekAlphabet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_interface_dependency_injection.CSharp.Main
+{
+    public class GreekAlphabet: IAlphabet
+    {
+        public Dictionary<Char, int> getLetterScores()
+        {
+            Dictionary<char, int> words = new Dictionary<char, int>
+            {
+                {'α', 1}, {'ε', 1}, {'ι', 1}, {'ο', 1}, {'φ', 1}, {'ξ', 1},
+                {'β', 2}, {'δ', 2}, {'π', 2},
+                {'γ', 3}, {'η', 3}, {'ν', 3}, {'τ', 3},
+                {'ζ', 4}, {'κ', 4}, {'ρ', 4}, {'ψ', 4}, {'λ', 4},
+                {'θ', 5}, {'χ', 5},
+                {'μ', 8}, {'σ', 8},
+                {'υ', 10}, {'ω', 10}
+            };
+            return words;
+        }
+    }
+}
